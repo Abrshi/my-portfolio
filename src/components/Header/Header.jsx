83b9ac7a702d36bb0d5b678285bdc05b6332435f
Abrshi <abrhamyeshitl990@gmail.com>
2025-03-01 19:58:15 +0300

    chenge contact icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { motion } from "framer-motion"
-import linkden from'../../assets/img/linkdin.png'
-import facebook from'../../assets/img/facebook.png'
-import phone from'../../assets/img/phone.png'
-import gmail from'../../assets/img/gmail.png'
+
+import { FaLinkedin } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+// import linkden from'../../assets/img/linkdin.png'
+// import facebook from'../../assets/img/facebook.png'
+// import phone from'../../assets/img/phone.png'
+// import gmail from'../../assets/img/gmail.png'
 import styles from './header.module.css'
 import Nav from './Nav'
 function Header() {
@@ -16,10 +21,9 @@ function Header() {
         <div className={styles.info}>
         <span className={styles.name}>Abrhem Yeshitla</span>
         <div className={styles.socials}>
-        <a href="https://www.linkedin.com/in/abreham-yeshitila-00b3b5250" target="_blank"><img src={linkden} alt="linkden"  /></a>
-        <a href="tel:+251966394582" target="_blank"><img src={phone} alt="phone" /></a>
-        <a href="mailto:abrehamyeshitla990@gmail.com" target="_blank"><img src={gmail} alt="gmail" /></a>
-        <a href="https://www.facebook.com/abrham.yeshetila" target="_blank"><img src={facebook} alt="facebook" /></a>
+        <a href="https://www.linkedin.com/in/abreham-yeshitila-00b3b5250" target="_blank"><FaLinkedin /></a>
+        <a href="tel:+251966394582" target="_blank"><FaPhoneAlt /></a>
+        <a href="mailto:abrehamyeshitla990@gmail.com" target="_blank"><MdEmail/></a>
                </div>
       </div>
       </motion.div>
