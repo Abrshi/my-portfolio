2dafb992cb630404abcd8ed1dda9c9b94a7aad6a
Abrham Yeshitla <abrhamyeshitla990@gmail.com>
2025-05-23 23:48:01 +0300

    update on project

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -6,6 +6,8 @@ import applereact from'../../assets/workimg/apple-react.jpeg'
 import Book from'../../assets/workimg/Book.png'
 import Netflix from'../../assets/workimg/Netflix.jpeg'
 import portfolio from'../../assets/workimg/portfolio.png'
+import zemate from'../../assets/workimg/zemate.png'
+import ecommerce from'../../assets/workimg/ecommerce.png'
 // import Amezon from'../../assets/workimg/Amazon.jpeg'
 export default function Work() {
   return (
@@ -24,10 +26,24 @@ export default function Work() {
           demo_link={"https://main--amazon-clones-by-abrham.netlify.app/"}
           github_link={"https://github.com/abrshi/amazon"}
         />
+         <Card
+          className={style.cared}
+          title={"E-commerce"}
+          background_img={Netflix}
+          demo_link={"https://abrshi-ecommerce.netlify.app"}
+          github_link={"https://github.com/Abrshi/ecommerce"}
+        />
+         <Card
+          className={style.cared}
+          title={"zemate"}
+          background_img={zemate}
+          demo_link={"https://zemate.netlify.app/"}
+          github_link={"https://github.com/Abrshi/podcast"}
+        />
         <Card
           className={style.cared}
           title={"Netflex"}
-          background_img={Netflix}
+          background_img={ecommerce}
           demo_link={"https://abrshi.github.io/netflixx/"}
           github_link={"https://github.com/Abrshi/netflixx"}
         />
